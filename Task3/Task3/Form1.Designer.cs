79bfdcb0811a48f0c58caffca0952965e9ae64c2
akkgogi <akkgogi@yandex.ru>
2023-03-06 13:19:09 +0300

    Оно теперь работает

---
+++ Task3/Task3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(12, 216);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(414, 216);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(222, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.Location = new System.Drawing.Point(237, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.MainConvert);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(414, 189);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 24);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(12, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Что имеем";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(414, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Что хотим иметь";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(648, 442);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Конвертер валют по ЦБ РФ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Task3/Task3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public string dbname = "ValCur.db3";
+        public Form1()
+        {
+            string ParsingURL = @"https://cbr.ru/scripts/XML_daily.asp";
+            InitializeComponent();
+            CreateTable();
+            int Length = Parse(ParsingURL);
+            ComboBoxInitialising(Length);
+        }
+
+        private string CharParse(int id)
+        {
+            string output;
+
+            using (var db = new SQLiteConnection("Data Source=" + dbname))
+            {
+                string cmdtext = $"select charcode from valutes where id like {id}";
+                SQLiteCommand cmd = new SQLiteCommand(cmdtext, db);
+
+                db.Open();
+                output = (string)cmd.ExecuteScalar();
+                cmd.CommandText = $"select name from valutes where id like {id}";
+                output += " - " + (string)cmd.ExecuteScalar();
+            }
+            return output;
+        }
+
+        private void ComboBoxInitialising(int L)
+        {
+            for (int i = 0; i < L; i++)
+            {
+                comboBox1.Items.Add(CharParse(i));
+                comboBox2.Items.Add(CharParse(i));
+            }
+        }
+
+        private void CreateTable()
+        {
+            if (!File.Exists(dbname))
+            {
+                SQLiteConnection.CreateFile(dbname);
+            }
+
+            using (var db = new SQLiteConnection("Data Source=" + dbname))
+            { 
+                string cmdtext = "CREATE TABLE if not exists valutes(ID text primary key, nominal int not null, value double not null, charcode text not null, name text not null)";
+                SQLiteCommand cmd = new SQLiteCommand(cmdtext, db);
+
+                db.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private int Parse(string url)
+        {
+            var xml = XDocument.Load(url);
+            int count = 1;
+
+            using (var db = new SQLiteConnection("Data Source=" + dbname))
+            {
+                string cmdtext = "insert or replace into valutes(id, nominal, value, charcode, name) values(0, 1, 1, 'RUB', 'Российский рубль')";
+                db.Open();
+                SQLiteCommand cmd = new SQLiteCommand(cmdtext, db);
+                cmd.ExecuteNonQuery();
+
+                foreach (XElement Valute in xml.Element("ValCurs").Elements("Valute"))
+                {
+                    XElement CharCode = Valute.Element("CharCode");
+                    XElement Nominal = Valute.Element("Nominal");
+                    XElement Value = Valute.Element("Value");
+                    XElement Name = Valute.Element("Name");
+
+                    string ValueWithDot = "";
+                    for (int i = 0; i < Value.Value.Length; i++)
+                    {
+                        if (Value.Value[i] == ',')
+                        {
+                            ValueWithDot += '.';
+                        }
+                        else
+                        {
+                            ValueWithDot += Value.Value[i];
+                        }
+                    }
+
+                    cmdtext = $"insert or replace into valutes(ID, nominal, value, charcode, name) values({count}, {Nominal.Value},{ValueWithDot},'{CharCode.Value}','{Name.Value}')";
+                    cmd.CommandText = cmdtext;
+
+                    Console.WriteLine(cmd.CommandText);
+                    cmd.ExecuteNonQuery();
+
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private double GetCurs(int id)
+        {
+            using (var db = new SQLiteConnection("Data Source=" + dbname))
+            {
+                string cmdtext = $"select value from valutes where id like {id}";
+                SQLiteCommand cmd = new SQLiteCommand(cmdtext, db);
+
+                db.Open();
+                double value = (double)cmd.ExecuteScalar();
+
+                cmdtext = $"select nominal from valutes where id like {id}";
+                cmd.CommandText = cmdtext;
+                int nominal = (int)cmd.ExecuteScalar();
+
+                double result = Math.Round(value / nominal, 4);
+                return result;
+            }
+        }
+
+        private void MainConvert(object sender, EventArgs e)
+        {
+            int inputid;
+            int outputid;
+
+            try
+            {
+                inputid = comboBox1.SelectedIndex;
+                outputid = comboBox2.SelectedIndex;
+
+                double inputsum = Convert.ToDouble(textBox1.Text);
+
+                double inputcurs = GetCurs(inputid);
+                double outputcurs = GetCurs(outputid);
+
+                double resultcurs = inputcurs / outputcurs;
+
+                double resultsum = inputsum * resultcurs;
+
+                textBox2.Text = resultsum.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Введите сумму переводимой валюты!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
